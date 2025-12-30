5d9670d8b414ad3b52d975a86fbcec4dab70439e
Manjunath <manjumc071@gmail.com>
2025-12-30 16:10:51 +0530

    javascript

---
+++ assets/strings.js
@@ -0,0 +1,50 @@
+//============================= strings =============================
+
+const firstName = "John";
+const lastName = "Doe";
+const age = 30;
+
+const str = "Hello there my name is John Doe";
+const tags = "web design, web development, programming";
+
+let value ;
+
+// concatenation
+
+// value = firstName + " " + lastName;
+// console.log(value);
+
+//concatenation is also done using += operator and add 2 names and store it in the same variable
+
+// value = "hello, my name is " + firstName + " and I am " + age;
+// console.log(value);
+
+// escaping
+
+// value = "That's awesome, I can't wait";
+// console.log(value);
+
+// length property
+
+// value = firstName.length;
+// console.log(value);
+
+// concat() method
+
+// value = firstName.concat(" ", lastName);
+// console.log(value);
+
+// change case
+
+// value = firstName.toUpperCase();
+// value = firstName.toLowerCase();
+// console.log(value);
+
+// indexOf() method
+
+// value = firstName.indexOf("o"); //returns the first occurrence of the specified value
+
+// console.log(value);
+
+// value = firstName.lastIndexOf("o"); //returns the last occurrence of the specified value
+// console.log(value);
